23164cb020fc03041edafb86a3158b6ccb907e7f
koga7 <considerboy@gmail.com>
2022-03-24 22:18:07 +0900

    feat(su60/rules.mk): change bootloader & options
    changed bootloader to one for pro micro & delete options {MOUSEKEY, EXTRAKEY}

--- keyboards/su60/rules.mk
+++ keyboards/su60/rules.mk
@@ -2,14 +2,12 @@
 MCU = atmega32u4
 
 # Bootloader selection
-BOOTLOADER = atmel-dfu
+BOOTLOADER = caterina    # Pro Micro
 
 # Build Options
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
