c0a3da25b34a59c48d158d44d854c942a8b7baf2
koga7 <considerboy@gmail.com>
2022-03-24 23:16:41 +0900

    feat(su60/default/keymap.c): setting _BASE layout

--- keyboards/su60/keymaps/default/keymap.c
+++ keyboards/su60/keymaps/default/keymap.c
@@ -12,11 +12,19 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_A,    KC_1,    MO(_FN),
-            KC_TAB,   KC_SPC
+        KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_P4,   KC_P5,   KC_P6,   KC_PAST,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P0,   KC_UP,   KC_PDOT, KC_PPLS,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_GRV,  KC_SPC,  KC_ENT,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PENT
     ),
     [_FN] = LAYOUT(
-        _______, _______,  _______,
-            RESET,    XXXXXXX
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     )
 };
