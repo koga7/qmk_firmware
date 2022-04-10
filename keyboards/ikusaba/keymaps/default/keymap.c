4bfbc4ed7d734cae8245775a18a23d0286559c14
koga7 <considerboy@gmail.com>
2022-04-10 15:50:57 +0900

    feat(keyboard/ikusaba/keymaps): Add layer & RESET key

--- keyboards/ikusaba/keymaps/default/keymap.c
+++ keyboards/ikusaba/keymaps/default/keymap.c
@@ -5,8 +5,12 @@
 
 enum layer_number {
     _QWERTY = 0,
-    _ADJUST
+    _LOWER,
+    _RAISE
 };
+
+#define LOWER LT(_LOWER, KC_DEL)
+#define RAISE LT(_RAISE, KC_BSPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -22,10 +26,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------'      `-----------------------------------------'
      */
   [_QWERTY] = LAYOUT(
-                 KC_Q, KC_W, KC_E, KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_LSFT, KC_A, KC_S, KC_D, KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_SCLN,
-        KC_LCTL, KC_Z, KC_X, KC_C, KC_V,   KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                                   KC_DEL, KC_SPC, KC_ENT,  KC_BSPC
+                 KC_Q, KC_W, KC_E, KC_R,  KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_LSFT, KC_A, KC_S, KC_D, KC_F,  KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_SCLN,
+        KC_LCTL, KC_Z, KC_X, KC_C, KC_V,  KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+                                   LOWER, KC_SPC, KC_ENT,  RAISE
+      ),
+  [_LOWER] =  LAYOUT(
+               RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______
       ),
     /* Adjust
      * ,-----------------------------------------.      ,-----------------------------------------.
@@ -38,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |      |      |      |      |      |      |      |      |      |      |      |      |      |
      * `-----------------------------------------'      `-----------------------------------------'
      */
-  [_ADJUST] =  LAYOUT(
+  [_RAISE] =  LAYOUT(
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, _______, _______, _______, _______,
