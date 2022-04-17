8cedbdd5da41cee1344add833a3e14c39f126ad3
koga7 <considerboy@gmail.com>
2022-04-17 15:44:40 +0900

    feat(ikusaba & su60 keymap): change keymap

--- keyboards/ikusaba/keymaps/default/keymap.c
+++ keyboards/ikusaba/keymaps/default/keymap.c
@@ -6,17 +6,23 @@
 enum layer_number {
     _QWERTY = 0,
     _LOWER,
-    _RAISE
+    _RAISE,
+    _ADJUST
 };
 
 #define LOWER LT(_LOWER, KC_ENT)
-#define RAISE LT(_RAISE, A(KC_GRV))
+#define RAISE LT(_RAISE, KC_TAB)
+#define SHIFT SFT_T(KC_SPC)
+#define CTRL CTL_T(KC_DEL)
+#define WIN WIN_T(KC_Z)
+#define ALT ALT_T(KC_V)
+#define ZKHK C(KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* QWERTY
      * ,-----------------------------------------.      ,-----------------------------------------.
-     * | Tab  |   Q  |   W  |   E  |   R  |   T  |      |   Y  |   U  |   I  |   O  |   P  |   [  |
+     * | Tab  |   Q  |   P  |   E  |   R  |   T  |      |   Y  |   U  |   I  |   O  |   P  |   [  |
      * |------+------+------+------+------+------|      |------+------+------+------+------+------|
      * | Esc  |   A  |   S  |   D  |   F  |   G  |      |   H  |   J  |   K  |   L  |   ;  |   "  |
      * |------+------+------+------+------+------|      |------+------+------+------+------+------|
@@ -26,15 +32,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------'      `-----------------------------------------'
      */
   [_QWERTY] = LAYOUT(
-                 KC_Q, KC_W, KC_E, KC_R,  KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_LSFT, KC_A, KC_S, KC_D, KC_F,  KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_SCLN,
-        KC_LCTL, KC_Z, KC_X, KC_C, KC_V,  KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                                   LOWER, KC_SPC, KC_BSPC, RAISE
+              KC_Q, KC_P, KC_U,     KC_Y,  KC_COMM,  KC_J,  KC_D,   KC_H, KC_G, KC_QUOT,
+        ZKHK, KC_I, KC_O, KC_E,     KC_A,  KC_DOT,   KC_K,  KC_T,   KC_N, KC_S, KC_R,    KC_MINS,
+        CTRL, WIN,  KC_X, KC_SLSH,  KC_C,  KC_SCLN,  KC_M,  KC_L,   KC_F, KC_B, ALT,     KC_W,
+                                    SHIFT, LOWER,    RAISE, KC_BSPC
       ),
   [_LOWER] =  LAYOUT(
-               RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+              KC_1,    KC_2,  KC_3,    KC_4,   KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+      KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_CAPS,
+      KC_F1,  KC_F2,   KC_F3, KC_F4,   KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
       _______, _______, _______, _______
       ),
     /* Adjust
@@ -49,9 +55,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------'      `-----------------------------------------'
      */
   [_RAISE] =  LAYOUT(
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-      _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______
+                 _______, _______, KC_PLUS, KC_PIPE, KC_LABK, _______, KC_GRV,  KC_LCBR, KC_RCBR, KC_DQT,
+        _______, _______, _______, KC_EQL,  KC_BSLS, KC_RABK, KC_TILD, KC_UP,   KC_LBRC, KC_RBRC, _______, KC_UNDS,
+        _______, _______, _______, KC_QUES, _______, KC_COLN, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_EQL,  _______,
+        _______, _______, _______, _______
+      ),
+  [_ADJUST] =  LAYOUT(
+                 _______, _______, _______, _______, RESET,         S(C(KC_DEL)),  _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, G(C(KC_D)),    G(C(KC_F4)),   _______, _______, _______, _______, _______,
+        _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, G(C(KC_LEFT)), G(C(KC_RGHT)), _______, _______, _______, _______, _______,
+        _______, _______, _______, _______
       )
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
