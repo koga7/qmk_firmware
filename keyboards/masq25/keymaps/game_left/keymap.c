bb203709705dd54b99c90142cef996041471621d
koga7 <considerboy@gmail.com>
2022-12-07 11:24:28 +0900

    feat(masq25): rename keymap & map keys

--- keyboards/masq25/keymaps/game_left/keymap.c
+++ keyboards/masq25/keymaps/game_left/keymap.c
@@ -3,22 +3,22 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┬───┬
-     * │ A │ B │ C │ D │ E │
+     * │ 1 │ 2 │ 3 │ 4 │ 5 │
      * ├───┼───┼───┼───┼───┼
-     * │ A │ B │ C │ D │ E │
+     * │ 6 │ 7 │ 8 │ 9 │10 │
      * ├───┼───┼───┼───┼───┼
-     * │ A │ B │ C │ D │ E │
+     * │11 │12 │13 │14 │15 │
      * ├───┼───┼───┼───┼───┼
-     * │ A │ B │ C │ D │ E │
+     * │16 │17 │18 │19 │20 │
      * ├───┼───┼───┼───┼───┼
-     * │ A │ B │ C │ D │ E │
+     * │21 │22 │23 │24 │25 │
      * └───┴───┴───┴───┴───┴
      */
     [0] = LAYOUT_ortho_5x5(
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_ENT,  KC_SPC
     )
 };
