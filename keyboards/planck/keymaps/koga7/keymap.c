ec87504e035f30c7f73d5e3bd12d9fe43353373e
koga7 <considerboy@gmail.com>
2022-12-10 12:30:21 +0900

    feat(planck/koga7): add new keymap

--- keyboards/planck/keymaps/koga7/keymap.c
+++ keyboards/planck/keymaps/koga7/keymap.c
@@ -30,27 +30,27 @@ enum planck_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
-    JP_ZKHK,        JP_SCLN,      JP_COLN,      JP_MINS, JP_CIRC, JP_YEN,         JP_MHEN,             JP_HENK, JP_AT,   JP_LBRC, JP_RBRC, KC_DEL,
-    KC_TAB,         KC_Q,         KC_W,         KC_E,    KC_R,    KC_T,           JP_COMM,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    LCTL_T(KC_ESC), KC_A,         KC_S,         KC_D,    KC_F,    KC_G,           JP_DOT,              KC_H,    KC_J,    KC_K,    KC_L,    LT(_RAISE, JP_EXLM),
-    LSFT_T(KC_B),   LGUI_T(KC_Z), LALT_T(KC_X), KC_C,    KC_V,    LSFT_T(KC_SPC), LT(_RAISE, KC_ENT),  KC_BSPC, KC_N,    KC_M,    JP_SLSH, LT(_LOWER, JP_BSLS)
+    KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,           KC_TAB,             KC_EQL,  KC_Y,               KC_U,    KC_I,    KC_O,    KC_P,
+    KC_A,    KC_S,    KC_D,     KC_F,    KC_G,           LCTL_T(KC_ESC),     KC_MINS, KC_H,               KC_J,    KC_K,    KC_L,    KC_SCLN,
+    KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,           KC_QUOT,            KC_LBRC, KC_N,               KC_M,    KC_UP,   KC_COMM, KC_DOT,
+    KC_LCTL, KC_LGUI, KC_LALT,  KC_F13,  LSFT_T(KC_SPC), LT(_LOWER, KC_ENT), KC_BSPC, LT(_RAISE, KC_F14), KC_LEFT, KC_DOWN, KC_RGHT, KC_SLSH
 ),
 [_LOWER] = LAYOUT_planck_grid(
-    _______, JP_1,    JP_2,    JP_3,    JP_4,   JP_5,     JP_6,    JP_7,    JP_8,    JP_9,    JP_0,    _______,
-    _______, JP_EXLM, JP_DQUO, JP_HASH, JP_DLR, JP_PERC,  JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, _______, _______,
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______
 ),
 [_RAISE] = LAYOUT_planck_grid(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 /* Adjust (Lower + Raise)
  *                      v------------------------RGB CONTROL--------------------v
  * ,-----------------------------------------------------------------------------------.
- * |      | Reset|Debug | RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-|  Del |
+ * |      | Reset|Debug | RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-|NMLCK |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    _______, QK_BOOT, DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL,
+    _______, QK_BOOT, DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_NUM,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
@@ -75,3 +75,25 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
+
+enum combos{
+  DQUOS,
+  RBRCS,
+  COLNS,
+  BSLSS,
+  TILDS
+};
+
+const uint16_t PROGMEM dquo_combo[] = {KC_B, KC_QUOT, COMBO_END};
+const uint16_t PROGMEM rbrc_combo[] = {KC_LBRC, KC_N, COMBO_END};
+const uint16_t PROGMEM coln_combo[] = {KC_L, KC_SCLN, COMBO_END};
+const uint16_t PROGMEM bsls_combo[] = {KC_RGHT, KC_SLSH, COMBO_END};
+const uint16_t PROGMEM grv_combo[] = {KC_COMM, KC_DOT, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+  [DQUOS] = COMBO(dquo_combo, KC_DQUO),
+  [RBRCS] = COMBO(rbrc_combo, KC_RBRC),
+  [COLNS] = COMBO(coln_combo, KC_COLN),
+  [BSLSS] = COMBO(bsls_combo, KC_BSLS),
+  [TILDS] = COMBO(grv_combo, KC_GRV),
+  };
