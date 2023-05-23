cb60591036e3dea7934da4b95bfe1efd7d49cdbb
koga7 <considerboy@gmail.com>
2023-05-23 22:58:21 +0900

    fix(masq25/keymaps/default): fix default layout 5x5

--- keyboards/masq25/keymaps/default/keymap.c
+++ keyboards/masq25/keymaps/default/keymap.c
@@ -2,20 +2,23 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
+     * ┌───┬───┬───┬───┬───┬
+     * │ 1 │ 2 │ 3 │ 4 │ 5 │
+     * ├───┼───┼───┼───┼───┼
+     * │ 6 │ 7 │ 8 │ 9 │10 │
+     * ├───┼───┼───┼───┼───┼
+     * │11 │12 │13 │14 │15 │
+     * ├───┼───┼───┼───┼───┼
+     * │16 │17 │18 │19 │20 │
+     * ├───┼───┼───┼───┼───┼
+     * │21 │22 │23 │24 │25 │
+     * └───┴───┴───┴───┴───┴
      */
-    [0] = LAYOUT_ortho_4x4(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+    [0] = LAYOUT_ortho_5x5(
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_ENT,  KC_SPC
     )
 };
